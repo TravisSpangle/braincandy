022fc5ad37fa5a76c08db1f8a5ff2bb26724124a
Travis <Spangltk@gmail.com>
2015-08-15 09:03:02 -0700

    Code Review with RuboCop

--- lazy-professor/Lazy.rb
+++ lazy-professor/Lazy.rb
@@ -1,3 +1,4 @@
+# Lazy Class performs tasks for a Lazy Professor
 class Lazy
   attr_accessor :sample, :answers
 
@@ -5,24 +6,24 @@ class Lazy
     fail unless File.exist?(file)
 
     lazy = Lazy.new
-    
+
     lazy.sample = File.readlines(file)
-    lazy.sample.map!{ |line| line[0..-2] }
+    lazy.sample.map! { |line| line[0..-2] } # clean EOL
 
     lazy.load_answers
     lazy.parse_sample_for_answer
   end
 
   def load_answers
-    longest_length = sample.map{ |line| line.length}.sort.last
-    counter = Hash.new{ |hash,key| hash[key] = 0 } 
-    @answers = Array.new
-    longest_length.times{ @answers << counter.clone }
+    longest_length = sample.map(&:length).sort.last
+    counter = Hash.new { |hash, key| hash[key] = 0 }
+    @answers = []
+    longest_length.times { @answers << counter.clone }
   end
 
   def parse_sample_for_answer
     sample.each do |line|
-      line.chars.each_with_index do |char,index|
+      line.chars.each_with_index do |char, index|
         answers[index][char] += 1
       end
     end
@@ -31,11 +32,11 @@ class Lazy
   end
 
   def detect_highest_occurances
-    final = String.new()
+    final = ''
 
     answers.each do |ans|
-      final << ans.sort_by{ |name, occ| occ }.reverse[0][0]
+      final << ans.sort_by { |_, occurances| occurances }.reverse[0][0]
     end
-    return final
+    final
   end
 end

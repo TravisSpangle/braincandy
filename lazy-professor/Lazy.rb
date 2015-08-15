0ebd496ab7e2038c07f45a34262a39c777a8fb29
Travis <Spangltk@gmail.com>
2015-08-14 21:39:46 -0700

    supporting class methods

--- lazy-professor/Lazy.rb
+++ lazy-professor/Lazy.rb
@@ -1,20 +1,22 @@
 class Lazy
-  attr_reader :sample, :answers
+  attr_accessor :sample, :answers
 
   def self.answer(file)
     fail unless File.exist?(file)
+
+    lazy = Lazy.new
     
-    @sample = File.readlines(file)
-    @sample.map!{ |line| line[0..-2] }
-    parse_sample_for_answer
+    lazy.sample = File.readlines(file)
+    lazy.sample.map!{ |line| line[0..-2] }
+    lazy.parse_sample_for_answer
   end
 
   def self.test_answer(sample_data)
-    @sample = sample_data
-    parse_sample_for_answer
+    lazy = Lazy.new
+    
+    lazy.sample = sample_data
+    lazy.parse_sample_for_answer
   end
-
-protected
 
   def load_answers
     @answers = Array.new( sample.length, Hash.new{ |hash,key| hash[key] = 0 } )
@@ -27,10 +29,12 @@ protected
         answers[index][char] += 1
       end
     end
+
+    detect_highest_occurances
   end
 
   def detect_highest_occurances
-    final = Array.new()
+    final = String.new()
 
     answers.each do |ans|
       final << ans.sort_by{ |name, occ| occ }.reverse[0][0]

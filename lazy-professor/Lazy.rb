a535bd270c6faf142a2b022150259e64d13ca6c3
Travis <Spangltk@gmail.com>
2015-08-19 09:41:13 -0700

    optimizations
    Using string methods to clean urls. Separating concerns; Sample should
    be an array of char arrays, calculate_occurrences is responsible for
    prepping Answers.

--- lazy-professor/Lazy.rb
+++ lazy-professor/Lazy.rb
@@ -7,24 +7,27 @@ class Lazy
 
     lazy = Lazy.new
 
-    lazy.sample = File.readlines(file)
-    lazy.sample.map! { |line| line[0..-2] } # clean EOL
-
-    lazy.load_answers
+    lazy.load_from_file file
     lazy.calculate_occurrences
     lazy.detect_highest_occurrences
+  end
+
+  def load_from_file(file)
+    self.sample = File.readlines(file).map { |line| line.chomp.chars }
   end
 
   def load_answers
     longest_length = sample.map(&:length).sort.last
     counter = Hash.new { |hash, key| hash[key] = 0 }
+
     @answers = []
     longest_length.times { @answers << counter.clone }
   end
 
   def calculate_occurrences
+    lazy.load_answers
     sample.each do |line|
-      line.chars.each_with_index do |char, index|
+      line.each_with_index do |char, index|
         answers[index][char] += 1
       end
     end
